6b2c85f79c88a638ff2738139f3c15c610a75151
Alex Wallace <alexwallax@gmail.com>
2021-12-25 11:30:21 -0300

    Aulas js dio

---
+++ assets/js/script.js
@@ -0,0 +1,83 @@
+//vetores e arrays
+
+//let array = ['string', 1, true];
+
+let array = ['string', 1, true, ['array1'],['array2'],['array3'],['array4']];
+
+/*
+console.log(array);
+
+console.log(array[0]);
+
+console.log(array[3]);
+*/
+
+
+
+// forEach - percorre um array - nesse exemplo vai imprimir o item e o índece
+/*
+array.forEach(function(item, index){console.log(item, index)});
+*/
+
+
+//push - add novo item no final do array
+/*
+array.push('Novo item');
+console.log(array);
+*/
+
+
+//pop - remove um item no final do array
+/*
+array.pop();
+console.log(array);
+*/
+
+
+//shift - remove um item do início do array
+/*
+array.shift();
+console.log(array);
+*/
+
+
+//unshift - add o item no ínicio do array
+/*
+array.unshift('Novo item no ínicio');
+console.log(array);
+*/
+
+//indexOf - retorna o -indice de um item de um array
+/*
+console.log(array.indexOf(true));
+*/
+
+
+//splice - remove ou substutui um item pelo índice
+/*
+array.splice(0, 3); // vai apagar do índice (0 até 3)
+console.log(array);
+*/
+
+
+//slice - retorna uma parte de um array existente
+/*
+let novoArray = array.slice(0, 3); //vai pegar os item dos índeces do (0 até 3)
+console.log(novoArray);
+*/
+
+
+
+// objeto
+let objeto = { string: 'string', numbrt: 1, booleana: true, array: ["array"], objectInterno: {objectInterno: "objeto interno"}};
+
+//console.log(objeto);
+console.log(objeto.booleana);
+console.log(objeto.objectInterno);
+
+
+// buscando apenas as strings do array
+var string = objeto.string;
+console.log(string);
+
+
