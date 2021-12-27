96a853802c7d6dc5309765fa974abb719f5bac31
Alex Wallace <alexwallax@gmail.com>
2021-12-27 17:28:25 -0300

    Aula js DIO funções

---
+++ funcoes/script.js
@@ -0,0 +1,12 @@
+function funcao() {
+    console.log("Função teste");
+}
+
+funcao();
+
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
+}
+
+mensagem("primeiro parametro,", "segundo parametro");
