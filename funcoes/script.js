176479c4e7a012b2023f8f0ca210509a237f34e3
Alex Wallace <alexwallax@gmail.com>
2021-12-27 21:17:16 -0300

    Aula js DIO - criando uma calculadora com javascript

--- funcoes/script.js
+++ funcoes/script.js
@@ -1,12 +1,43 @@
-function funcao() {
-    console.log("Função teste");
-}
-
-funcao();
-
-
+//função com parametro
+/*
 function mensagem(primeiro, segundo) {
     console.log(primeiro, segundo);
 }
 
 mensagem("primeiro parametro,", "segundo parametro");
+*/
+
+//---------------------------------------------------------
+
+//função declarativa
+function funcao() {
+    console.log("Função teste");
+}
+
+funcao();
+//---------------------------------------------------------
+
+
+
+//expressões de funções
+var funcao02 = function funcao02() {
+    console.log("Função de expressão");
+}
+
+funcao02();
+
+//expressões de funções sem nomeação
+var funcao03 = function() {
+    console.log("Função de expressão sem nomeação");
+}
+
+funcao03();
+
+
+//---------------------------------------------------------
+//arrow function
+var funcao04 = () => {
+    console.log("Arrow Função");
+}
+
+funcao04();
