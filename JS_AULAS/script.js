dacd0f4c24ca57320acbad79cb43faec0567a8b8
Alex Wallace <alexwallax@gmail.com>
2021-12-28 10:08:53 -0300

    Aula js DIO - Object

---
+++ JS_AULAS/script.js
@@ -0,0 +1,29 @@
+// object 
+let user = {
+    name: 'Alex'
+};
+
+//alterando propriedade do objeto
+console.log(user);
+user.name = 'Wallace';
+console.log(user);
+
+//alterando propriedade do objeto outra forma
+user['name'] = 'Outro nome';
+console.log(user);
+
+
+const prop = 'nome';
+user[prop] = 'Outro nome 2';
+
+console.log(user);
+
+// criando uma propriedade
+user.lastName = 'Souza de Araujo';
+
+console.log(user);
+
+// deletando uma propriedade
+delete user.name;
+
+console.log(user);
